5d34d7b849e9fd31f9383822fb32df31ab8d3465
syonosuke743 <syonosuke1996@outlook.jp>
2024-03-20 08:40:03 +0900

    puma.rbの編集

--- config/puma.rb
+++ config/puma.rb
@@ -11,6 +11,7 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
+
 # Specifies that the worker count should equal the number of processors in production.
 if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
@@ -23,7 +24,7 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+bind "unix:///var/www/realworld_api/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
